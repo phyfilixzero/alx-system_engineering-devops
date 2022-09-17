b242e84531957c35ff7f6cbd5bad3147f092b9c1
uche felix <felixuc2580@gmail.com>
2022-09-17 17:36:00 +0100

    edit 2

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# Create a file
-file { '/tmp/holberton':
-  ensure  => file,
+# create a file in /tmp with specific requirements
+
+file { 'holberton':
   path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet',
 }
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Install puppet-lint
+# install puppet-lint
+
 package { 'puppet-lint':
   ensure   => '2.1.1',
-  name     => 'puppet-lint',
-  provider => 'gem'
+  provider => 'gem',
 }
